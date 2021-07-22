d7d7c49460cf2483ce403750b8bd27142e7c34fa
Haleym Hidalgo <hhidalgo@webclass.com>
2021-07-22 16:32:26 -0400

    Archivos del proyecto

---
+++ src/components/PxButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    @click="buttonClick"
+    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent rounded"
+  >
+    <beat-loader :loading="isLoading" :color="'#2E86C1 '" :size="8" />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    buttonClick() {
+      this.$emit('custom-click')
+    }
+  }
+}
+</script>
